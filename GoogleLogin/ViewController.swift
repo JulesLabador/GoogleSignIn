4aa6d7abfa69e2ca5413dbd58ba6acd02ab86bb2
Jules <jlabador@gmail.com>
2019-02-06 19:21:38 -0800

    UI adjustments.

--- GoogleLogin/ViewController.swift
+++ GoogleLogin/ViewController.swift
@@ -37,8 +37,8 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
             let familyName = user.profile.familyName ?? ""
             let email = user.profile.email ?? ""
             
-            self.nameLabel.text = "FULL NAME: \(fullName)"
-            self.emailLabel.text = "EMAIL: \(email)"
+            self.fetchedNameLabel.text = "\(fullName)"
+            self.fetchedEmailLabel.text = "\(email)"
             
             print(userId)
             print(idToken)
@@ -62,11 +62,30 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
         
         return button
     }()
-    let nameLabel: UILabel = {
+    let fullNameLabel: UILabel = {
        
         let label = UILabel()
         label.text = "FULL NAME"
-        label.font = UIFont(name: "Avenir-Heavy", size: 20)
+        label.font = UIFont(name: "Avenir-Heavy", size: 15)
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    let fetchedNameLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = " "
+        label.font = UIFont(name: "Avenir-Medium", size: 22)
+        label.textAlignment = .center
+        label.backgroundColor = UIColor.white
+        label.layer.cornerRadius = 6
+        
+        label.layer.shadowRadius = 10
+        label.layer.shadowColor = UIColor.rgb(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        label.layer.shadowOpacity = 0.25
+        label.layer.shadowOffset = CGSize(width: 0.5, height: 3)
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -75,7 +94,26 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
         
         let label = UILabel()
         label.text = "EMAIL"
-        label.font = UIFont(name: "Avenir-Heavy", size: 20)
+        label.font = UIFont(name: "Avenir-Heavy", size: 15)
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    let fetchedEmailLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = " "
+        label.font = UIFont(name: "Avenir-Medium", size: 22)
+        label.textAlignment = .center
+        label.backgroundColor = UIColor.white
+        label.layer.cornerRadius = 6
+        
+        label.layer.shadowRadius = 10
+        label.layer.shadowColor = UIColor.rgb(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        label.layer.shadowOpacity = 0.25
+        label.layer.shadowOffset = CGSize(width: 0.5, height: 3)
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -88,13 +126,25 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
         signInButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         signInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64).isActive = true
         
-        view.addSubview(nameLabel)
-        nameLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 16).isActive = true
-        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        view.addSubview(fullNameLabel)
+        fullNameLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 16).isActive = true
+        fullNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        
+        view.addSubview(fetchedNameLabel)
+        fetchedNameLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 4).isActive = true
+        fetchedNameLabel.leftAnchor.constraint(equalTo: fullNameLabel.leftAnchor).isActive = true
+        fetchedNameLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -16).isActive = true
+        fetchedNameLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
         
         view.addSubview(emailLabel)
-        emailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
-        emailLabel.leftAnchor.constraint(equalTo: nameLabel.leftAnchor).isActive = true
+        emailLabel.topAnchor.constraint(equalTo: fetchedNameLabel.bottomAnchor, constant: 32).isActive = true
+        emailLabel.leftAnchor.constraint(equalTo: fullNameLabel.leftAnchor).isActive = true
+        
+        view.addSubview(fetchedEmailLabel)
+        fetchedEmailLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 4).isActive = true
+        fetchedEmailLabel.leftAnchor.constraint(equalTo: emailLabel.leftAnchor).isActive = true
+        fetchedEmailLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -16).isActive = true
+        fetchedEmailLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
     }
     
 }
@@ -102,5 +152,9 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
 extension UIColor {
     class func rgb(red: Int, green: Int, blue: Int) -> UIColor {
         return UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1)
+    }
+    
+    class func rgb(red: Int, green: Int, blue: Int, alpha: Float) -> UIColor {
+        return UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: CGFloat(alpha))
     }
 }
